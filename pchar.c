fa6c7172c8f3f4f37571dea4902c0c1b73e451ca
Avoka99 <benjaminlahadi99@gmail.com>
2023-12-13 16:41:08 -0800

    Stacks and Queues

--- pchar.c
+++ pchar.c
@@ -1,21 +1,23 @@
 #include "monty.h"
 
 /**
- *  * pchar - prints the char at the top of the stack
- *   * @stack: double pointer to the head of the stack
- *    * @line_number: line number in the Monty byte code file
- *     */
+ * pchar - prints the char at the top of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number in the Monty byte code file
+ */
 void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
